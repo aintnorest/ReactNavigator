787724da42dd6fe5d6d983b881e819899b7c0d3c
ChrisAlatorre <cjalatorre@gmail.com>
2016-02-23 23:54:10 -0800

    clearing the slate before I push an update.

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-//
-import navigator           from './navigator';
-
-
-export default combineReducers({
-    navigator: navigator("_BASE"),
-    
-});
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-require('babel-core/register');
-
-//process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-
-
-const config   = require('./build/config');
-console.log('ENV [', config.get('env'), ']');
-module.exports = require('./build/webpack/' + config.get('env'));
